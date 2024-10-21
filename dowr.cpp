bafa10eebbd3e831c2746e80913b4b41762a16ff
praetortuicu <teodor.ticu@outlook.com>
2024-10-21 15:47:41 +0300

    First test didn't go so well but the basic idea is here

--- dowr.cpp
+++ dowr.cpp
@@ -4,8 +4,8 @@
 #include <string>
 #include <regex>
 
-#define NAME_MARK "_name" 
-#define PARAMETER_MARK "_param"
+#define NAME_MARK "_name_" 
+#define PARAMETER_MARK "_param_"
 
 std::fstream readFileObject;
 std::fstream writeFileObject;
@@ -16,8 +16,8 @@ void read_new_file(std::string fileName){
 	}
 	else{
 		printf("Error! File already opened...\n");
-		return ;
 	}
+	return ;
 }
 
 void close_opened_file(std::string fileName){
@@ -26,9 +26,9 @@ void close_opened_file(std::string fileName){
 		readFileObject.close();
 	}
 	else{
-		printf("Could not open file! Already closed...\n");
-		return ;
+		printf("Already closed...\n");
 	}
+	return ;
 }
 
 void open_write_file(std::string fileName){
@@ -40,25 +40,38 @@ void open_write_file(std::string fileName){
 	}
 	else{
 		printf("Failed to open doc file! File to read couldn't open...");
-		return ;
 	}
+	return ;
+}
+void close_write_file(std::string fileName){
+	if(writeFileObject.is_open()){
+		printf("Closing file...");
+		writeFileObject.close();
+	}
+	else{
+		printf("Write file already closed!");	
+	}
+	return;
 }
 
 int main(int argc, char *argv[]){
 	std::string fileName = argv[2];
 	read_new_file(fileName);
-	open_write_file(fileName);
+	std::string fileNameDoc = fileName.append("DOC");
+	open_write_file(fileNameDoc);
 	std::string line;
 	std::smatch name_match;
 	std::smatch param_match;
 	std::regex marks(NAME_MARK, PARAMETER_MARK);
 	if(readFileObject.is_open() && !readFileObject.eof()){
 		while(getline(readFileObject, line)){
-			std::regex_search(line, name_match, marks);
-	}	
+			if(std::regex_search(line, name_match, marks)){
+				writeFileObject << line << "\n";	
+			}
+		}	
 }
 
-
-	close_opened_file(fileName);
+	close_write_file(fileName);
+	close_opened_file(fileNameDoc);
 	return 0;
 }
